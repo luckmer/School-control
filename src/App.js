543179c12f323af686c6c0b49bcc638dd8f72413
luckmer <woszczo04@gmail.com>
2020-09-15 12:16:59 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Switch, Route, useLocation } from "react-router-dom";
+import { routes } from "./routes";
+import { Store } from "./Imports/Index";
+import NavBar from "./components/NavBar";
+import { ThemeProvider } from "styled-components";
+import DarkMode from "./hooks/DarkMode";
+import Box from "./styles/Box";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const { Color, toggleChange } = DarkMode();
 
+    const location = useLocation();
+    return (
+        <ThemeProvider theme={{ mode: Color }}>
+            <Box>
+                <Store>
+                    <NavBar type={Color} toggleChange={toggleChange} />
+                    <Switch location={location} key={location.pathname}>
+                        {routes.map(({ path, Component }) => (
+                            <Route
+                                key={path}
+                                exact
+                                path={path}
+                                component={Component}
+                            />
+                        ))}
+                    </Switch>
+                </Store>
+            </Box>
+        </ThemeProvider>
+    );
+}
 export default App;
