55ffa63c576c9c8c645482d5a9eef07b22698a71
luckmer <woszczo04@gmail.com>
2020-09-15 12:16:23 +0200

    Store

---
+++ src/store/Store.js
@@ -0,0 +1,40 @@
+import React, { createContext, useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+
+export const StoreContext = createContext();
+
+export default ({ children }) => {
+    const initialState = {
+        id: nanoid(),
+        Subject: "",
+        Teacher: "",
+        Mark: "",
+        Description: "",
+    };
+
+    const [state, setState] = useState(initialState);
+    const [table, setTable] = useState({
+        data: [],
+    });
+    const [Solution, setSolution] = useState({
+        find: [],
+    });
+
+    const store = {
+        TEXT: [state, setState],
+        DATA: [table, setTable],
+        SOLUTION: [Solution, setSolution],
+    };
+    console.log(Solution);
+    useEffect(() => {
+        const store = JSON.parse(localStorage.getItem("DATAContext"));
+        if (store) setTable(store);
+    }, []);
+    useEffect(() => {
+        localStorage.setItem("DATAContext", JSON.stringify(table));
+    }, [table]);
+
+    return (
+        <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+    );
+};
