b0e3f0d64b9f655fa312fa3a1abea48a4695574a
luckmer <woszczo04@gmail.com>
2020-11-03 21:49:46 +0100

    CreateNewTaskSlice

---
+++ src/reducers/CreateNewTaskSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+const CreateNewTaskSlice = createSlice({
+    name: "CreateNewTaskSlice",
+    initialState,
+    reducers:{},
+});
+
+
+export default CreateNewTaskSlice.reducer;
