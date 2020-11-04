fdc3ddd6ad848bd8b9c00f079d5069d9f60a486f
luckmer <woszczo04@gmail.com>
2020-11-04 22:24:34 +0100

    index update ✨✨🎉🎊

--- src/reducers/Index.js
+++ src/reducers/Index.js
@@ -2,10 +2,10 @@ import { combineReducers } from "redux";
 import DeleteEditSlice from "./DeleteEditSlice";
 import DarkModeSlice from "./DarkModeSlice"
 import CreateNewTaskSlice from "./CreateNewTaskSlice"
-import OnOffSlice from "../reducers/OnOffSlice";
+import ContextSlice from "./ContextSlice";
 
 const Index = combineReducers({
-    OnOffSlice:OnOffSlice,
+    ContextSlice:ContextSlice,
     DeleteEditSlice: DeleteEditSlice,
     DarkModeSlice: DarkModeSlice,
     CreateNewTaskSlice: CreateNewTaskSlice,
