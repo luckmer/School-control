e998897870eafc8ee46a358811aafbce243d0a78
luckmer <woszczo04@gmail.com>
2020-11-06 21:53:22 +0100

    update  🎃🎃

--- src/reducers/CreateNewTaskSlice.js
+++ src/reducers/CreateNewTaskSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    Data:[]
+    Data: [],
+    filteredData:[]
 }
 const CreateNewTaskSlice = createSlice({
     name: "CreateNewTaskSlice",
@@ -12,17 +13,25 @@ const CreateNewTaskSlice = createSlice({
                 state.Data.push(action.payload)
             },
             prepare: (data) => {
-                return { payload: {data} }
+                return { payload: data }
             },
         },
         DeleteTask: (state) =>{
             state.Data.splice(state, 1);
         },
+        FilterData: (state, action) =>{
+            const { data } = action.payload;
+            state.filteredData = data
+        },
         EditTask:(state,action)=>{
-            
+            const { id, newContext } = action.payload;
+            const paymentId = state.Data.find(({data})=>data.id === id)
+            if (paymentId) {
+                paymentId.data = newContext
+            }
         }
     },
 });
 
-export const { CreateTask,DeleteTask } = CreateNewTaskSlice.actions;
+export const { CreateTask,DeleteTask,EditTask,FilterData } = CreateNewTaskSlice.actions;
 export default CreateNewTaskSlice.reducer;
