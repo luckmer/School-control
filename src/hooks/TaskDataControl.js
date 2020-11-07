90d4fd7f663d262fe5beb25fd00004d5109fb4d7
luckmer <woszczo04@gmail.com>
2020-11-07 11:43:48 +0100

    .

--- src/hooks/TaskDataControl.js
+++ src/hooks/TaskDataControl.js
@@ -21,7 +21,6 @@ export function TaskDataControl(){
     };
 
     const handleUpload = () => {
-       // const { data } = table;
         const Next = {
             id: nanoid(),
             Subject: Subject,
