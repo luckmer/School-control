8849afb1826e03e848f1f967859297d600f38f95
luckmer <woszczo04@gmail.com>
2020-11-07 11:43:42 +0100

    pages refactor

--- src/hooks/PaginatingControl.js
+++ src/hooks/PaginatingControl.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-export default function PaginatingControl({ LimitControl, find }) {
+export default function PaginatingControl({ LimitControl, state }) {
     const [page, setPage] = useState(1);
 
     const Length = page * LimitControl;
     const Post = Length - LimitControl;
-    const ContextView = find.slice(Post, Length);
+    const ContextView = state.slice(Post, Length);
     const paginate = (i) => setPage(i);
     return { ContextView, paginate, page };
 }
