cd88ac41d82febf803ab00aee2e9ac0df1f982a3
luckmer <woszczo04@gmail.com>
2020-11-10 23:19:32 +0100

    Context update 🧡🧡🧡🤍

--- src/components/Context.js
+++ src/components/Context.js
@@ -1,88 +1,23 @@
 import React,{useState}  from "react";
-import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import EditName from "../components/Edit";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import { IsEditing } from "../actions/SetEditOn";
+import { NavFilter,MappingData } from "../Imports/Index";
 
-const  ContextPanel = ({id , subject , teacher,mark,description}) =>{
-    const [edit, setEdit] = useState(false);
+const ContextPanel = ({ finder,Mapping }) =>{
+    const [data,setData] = useState([])
 
-    const handleClick = () =>{
-        setEdit(!edit)
-    }
-    const Edit = EditName({ id, subject, teacher, mark, description, IsEditing,edit,setEdit,handleClick });
+    NavFilter(Mapping, finder, setData);
     
 
-    const View = (
-        <Card
-            border="light "
-            style={{ boxShadow: " -6px 1px 11px rgba(0,0,0,0.25)" }}
-        >
-            <Link to={`/details/${id}`}>
-                <Card.Header
-                    style={{
-                        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
-                        backgroundColor: `${
-                            mark >= 3
-                                ? " #28a745"
-                                : "#28a745" || mark === 0
-                                ? "#6c757d"
-                                : "#6c757d"
-                        }`,
-                        color: "white",
-                    }}
-                >
-                    <label htmlFor={id}>{subject}</label>
-                </Card.Header>
-            </Link>
-            <Card.Body
-                onClick={() => handleClick(id)}
-                style={{
-                    cursor: "pointer",
-                    backgroundColor: `${
-                        mark >= 3
-                            ? " #28a745"
-                            : "#28a745" ||mark === 0
-                            ? "#6c757d"
-                            : "#6c757d"
-                    }`,
-                    color: "white",
-                }}
-            >
-                <Card.Title>
-                    <label htmlFor={id}>{teacher}</label>
-                </Card.Title>
-                <Card.Text
-                    style={{
-                        textOverflow: "ellipsis",
-                        whiteSpace: "nowrap",
-                        overflow: "hidden",
-                        width: "98%",
-                    }}
-                >
-                    <label htmlFor={description}>{description}</label>
-                </Card.Text>
-            </Card.Body>
-            <Card.Header
-                style={{
-                    backgroundColor: `${
-                        mark >= 3
-                            ? " #28a745"
-                            : "#28a745" || mark === 0
-                            ? "#6c757d"
-                            : "#6c757d"
-                    }`,
-                    color: "white",
-                    boxShadow: "rgba(0, 0, 0, 0.25) 0px -1px 1px",
-                }}
-            >
-                <label htmlFor={id}>{mark}</label>
-            </Card.Header>
-        </Card>
-    );
-
-    return !edit ? View : Edit
+    return (
+        <>
+        {data.map(({ id, Subject, Mark,Teacher }) => (
+            <MappingData id={id} subject={Subject} mark={Mark} teacher={Teacher}  key ={id} />
+        ))}
+        </>
+    )
 }
 
 export default ContextPanel;
+
+
