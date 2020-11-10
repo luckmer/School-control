0aab2d5de14426afcd853af54162f8613a317ad8
luckmer <woszczo04@gmail.com>
2020-11-10 23:20:23 +0100

    index update 🧵🧶

--- src/Imports/Index.js
+++ src/Imports/Index.js
@@ -4,3 +4,4 @@ export { default as PaginatingControl } from "../hooks/PaginatingControl.js";
 export { default as Paginating } from "../components/Paginating.js";
 export { default as NavFilter } from "../hooks/NavFilter";
 export { default as DetailsContext } from "../components/DetailsContext";
+export { default as MappingData } from "../components/MappingData";
