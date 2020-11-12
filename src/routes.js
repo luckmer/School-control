a159a61addc6f7f17726a79696588ebc7b86c3be
luckmer <woszczo04@gmail.com>
2020-11-12 21:50:11 +0100

    new page

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import Details from "./pages/Details.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 import NotFound from "./pages/404";
+import Notes from "./pages/Notes";
+
 export const routes = [
     { path: "/", Component: LandingPage, name: "home" },
+    {path: "/notes", Component: Notes, name: "notes"},
     { path: "/details/:id", Component: Details, name: "details" },
     { path: "*", Component: NotFound, name: "notFound" },
 ];
